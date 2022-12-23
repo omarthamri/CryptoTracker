9b1ae61695f8521db27ec38a146be64eda3212d8
Omar Thamri <omarthamri4@gmail.com>
2022-12-23 02:33:47 +0100

    7: Add a reusable Networking layer

---
+++ CryptoTracker/Utilities/NetworkingManager.swift
@@ -0,0 +1,51 @@
+//
+//  NetworkingManager.swift
+//  CryptoTracker
+//
+//  Created by omar thamri on 23/12/2022.
+//
+
+import Foundation
+import Combine
+
+class NetworkingManager {
+    
+    enum NetworkingError: LocalizedError {
+        case badUrlResponse(url: URL)
+        case unknown
+        
+        var errorDescription: String? {
+            switch self {
+            case .badUrlResponse(url: let url): return "Bad response from URL \(url)"
+            case .unknown: return "unkown error occured"
+            }
+        }
+    }
+    
+    static func download(url: URL) -> AnyPublisher<Data, Error> {
+        
+       return URLSession.shared.dataTaskPublisher(for: url)
+            .subscribe(on: DispatchQueue.global(qos: .default))
+            .tryMap ({try handleUrlResponse(output: $0, url: url)})
+            .receive(on: DispatchQueue.main)
+            .eraseToAnyPublisher()
+    }
+    
+    static func handleUrlResponse(output: URLSession.DataTaskPublisher.Output,url: URL) throws -> Data {
+        guard let response = output.response as? HTTPURLResponse, response.statusCode >= 200 && response.statusCode < 300 else {
+            throw NetworkingError.badUrlResponse(url: url)
+        }
+        return output.data
+    }
+    
+    static func handleCompletion(completion: Subscribers.Completion<Error>) {
+        switch completion {
+        case .finished:
+            break
+        case .failure(let error):
+            print(error.localizedDescription)
+        }
+    }
+    
+    
+}
