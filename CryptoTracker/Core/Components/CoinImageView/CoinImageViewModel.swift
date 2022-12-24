8b07384c21a98f2de7164ac9515391aa2d433478
Omar Thamri <omarthamri4@gmail.com>
2022-12-24 03:55:58 +0100

    8: Downloading Coin Images using Combine, MVVM, and a Networking Layer | SwiftUI Crypto App

---
+++ CryptoTracker/Core/Components/CoinImageView/CoinImageViewModel.swift
@@ -0,0 +1,35 @@
+//
+//  CoinImageViewModel.swift
+//  CryptoTracker
+//
+//  Created by omar thamri on 24/12/2022.
+//
+
+import Foundation
+import SwiftUI
+import Combine
+
+class CoinImageViewModel: ObservableObject {
+    @Published var image: UIImage? = nil
+    @Published var isLoading = false
+    private let coin: CoinModel
+    private let dataService: CoinImageService
+    private var cancellables = Set<AnyCancellable>()
+    
+    init(coin: CoinModel) {
+        self.coin = coin
+        self.dataService = CoinImageService(coin: coin)
+        self.addSubscribers()
+    }
+    
+    private func addSubscribers() {
+        dataService.$image
+            .sink { [weak self] (_) in
+                self?.isLoading = false
+            } receiveValue: { [weak self] (returnedImage) in
+                self?.image = returnedImage
+            }
+            .store(in: &cancellables)
+
+    }
+}
